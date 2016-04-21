84eae2f838bb88e7ac5dac370801812447ecbed6
Devin Hughes <devin@jd2.com>
2016-04-21 18:07:14 -0400

    Adding tools for compiling kernel and u-boot

--- build_docker_image.sh
+++ build_docker_image.sh
@@ -10,14 +10,20 @@ USR=`who mom likes | awk '{print $1}'`
 MAINTAINER="Devin Hughes <devin@jd2.com>"
 D_IMAGE_NAME="dkhughes/ubker:latest"
 D_SHARE_PATH=/home/"$USR"/docker-share/ubker
+D_UBOOT_TOOLS_RPATH=u-boot-xlnx/tools
+D_LINUX_DTC_RPATH=linux-xlnx/scripts/dtc
 
-# Populate the docker files with the config
-sed "s|@maintainer@|$MAINTAINER|" \
+# Populate the docker file with the config
+sed -e "s|@maintainer@|$MAINTAINER|" \
+    -e "s|@u-boot-tools@|$D_UBOOT_TOOLS_RPATH|" \
+    -e "s|@linux-dtc@|$D_LINUX_DTC_RPATH|" \
     "$DIR"/Dockerfile.in > \
     "$DIR"/Dockerfile
 
 # Now build the base image
-docker build -t "$D_IMAGE_NAME" .
+docker build --build-arg b_uid=`id -u $USR` \
+    --build-arg b_gid=`id -g $USR` \
+    -t "$D_IMAGE_NAME" .
 
 if [ $? -ne 0 ]; then
     echo ERROR: Failed to build image.
@@ -32,7 +38,8 @@ cd "$DIR"
 cat << EOF > run_terminal.sh
 #!/bin/sh
 # Runs to terminal and cleans up when done.
-docker run --rm -ti -v $D_SHARE_PATH:/opt/share \
+docker run --rm -ti --user developer \
+-v $D_SHARE_PATH:/home/developer \
 $D_IMG_SNAME
 
 EOF
